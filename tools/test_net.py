2bb57601f9ea1d017c894860f1488169fff0c2bf
zlagpacan <zlagpacan@gmail.com>
2023-11-16 00:13:05 -0500

    undo

--- tools/test_net.py
+++ tools/test_net.py
@@ -33,7 +33,6 @@ from fsdet.evaluation import (
     verify_results,
 )
 
-from detectron2.utils.logger import setup_logger
 
 class Trainer(DefaultTrainer):
     """
@@ -51,9 +50,6 @@ class Trainer(DefaultTrainer):
         For your own dataset, you can simply create an evaluator manually in your
         script and do not have to worry about the hacky if-else logic here.
         """
-        # print(f"\nMY PRINT: dataset_name = {dataset_name}\n")
-        logger = setup_logger(output_dir, distributed_rank=rank)
-        logger.info(f"\nMY PRINT: dataset_name = {dataset_name}\n")
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
         evaluator_list = []
